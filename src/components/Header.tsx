e25020913e93a00c4eced80a425dd6552c1ae3b1
Manuel Carrascal <tricdiart@gmail.com>
2024-06-09 23:54:49 -0500

    Migration to useReducer complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,13 @@
-import { useMemo } from 'react';
-import type { CartItem, Guitar } from '../types';
+import { Dispatch, useMemo } from 'react';
+import type { CartItem } from '../types';
+import { cartActions } from '../reducers/cart-reducer';
 
 type HeaderProps = {
 	cart: CartItem[];
-	removeFromCart: (id: Guitar['id']) => void;
-	decreaseQuantity: (id: Guitar['id']) => void;
-	increaseQuantity: (id: Guitar['id']) => void;
-	clearCart: () => void;
+	dispatch: Dispatch<cartActions>;
 };
 
-export default function Header({
-	cart,
-	removeFromCart,
-	decreaseQuantity,
-	increaseQuantity,
-	clearCart,
-}: HeaderProps) {
+export default function Header({ cart, dispatch }: HeaderProps) {
 	// State Derivado
 	const isEmpty = useMemo(() => cart.length === 0, [cart]);
 	const cartTotal = useMemo(
@@ -78,7 +70,12 @@ export default function Header({
 															<button
 																type="button"
 																className="btn btn-dark"
-																onClick={() => decreaseQuantity(guitar.id)}
+																onClick={() =>
+																	dispatch({
+																		type: 'decrease-quantity',
+																		payload: { id: guitar.id },
+																	})
+																}
 															>
 																-
 															</button>
@@ -86,7 +83,12 @@ export default function Header({
 															<button
 																type="button"
 																className="btn btn-dark"
-																onClick={() => increaseQuantity(guitar.id)}
+																onClick={() =>
+																	dispatch({
+																		type: 'increase-quantity',
+																		payload: { id: guitar.id },
+																	})
+																}
 															>
 																+
 															</button>
@@ -95,7 +97,12 @@ export default function Header({
 															<button
 																className="btn btn-danger"
 																type="button"
-																onClick={() => removeFromCart(guitar.id)}
+																onClick={() =>
+																	dispatch({
+																		type: 'remove-from-cart',
+																		payload: { id: guitar.id },
+																	})
+																}
 															>
 																X
 															</button>
@@ -113,7 +120,7 @@ export default function Header({
 
 								<button
 									className="btn btn-dark w-100 mt-3 p-2"
-									onClick={clearCart}
+									onClick={() => dispatch({ type: 'clear-cart' })}
 								>
 									Vaciar Carrito
 								</button>
--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -13,12 +13,17 @@ export type CartState = {
 	cart: CartItem[];
 };
 
-export const initialState: CartState = {
-	data: db,
-	cart: [],
+const initialCart = (): CartItem[] => {
+	const localStorageCart = localStorage.getItem('cart');
+	return localStorageCart ? JSON.parse(localStorageCart) : [];
 };
 
-//const MIN_ITEMS = 1;
+export const initialState: CartState = {
+	data: db,
+	cart: initialCart(),
+};
+
+const MIN_ITEMS = 1;
 const MAX_ITEMS = 5;
 
 export const cartReducer = (
@@ -58,21 +63,48 @@ export const cartReducer = (
 				cart: updatedCart,
 			};
 		}
-		case 'remove-from-cart':
+		case 'remove-from-cart': {
+			const cart = state.cart.filter((item) => item.id !== action.payload.id);
 			return {
 				...state,
+				cart: cart,
 			};
-		case 'decrease-quantity':
+		}
+		case 'decrease-quantity': {
+			const cart = state.cart.map((item) => {
+				if (item.id === action.payload.id && item.quantity > MIN_ITEMS) {
+					return {
+						...item,
+						quantity: item.quantity - 1,
+					};
+				}
+				return item;
+			});
+
 			return {
 				...state,
+				cart,
 			};
-		case 'increase-quantity':
+		}
+		case 'increase-quantity': {
+			const cart = state.cart.map((item) => {
+				if (item.id === action.payload.id && item.quantity < MAX_ITEMS) {
+					return {
+						...item,
+						quantity: item.quantity + 1,
+					};
+				}
+				return item;
+			});
 			return {
 				...state,
+				cart,
 			};
+		}
 		case 'clear-cart':
 			return {
 				...state,
+				cart: [],
 			};
 	}
 };
